d9947fafa09f742f0bf884e3bab8924ad634d33b
Joshua Schmid <jschmid@suse.com>
2014-10-16 18:12:07 +0200

    add listing and connection template

--- lib/ssh/manager/client.rb
+++ lib/ssh/manager/client.rb
@@ -1,10 +1,27 @@
-require_relative "../manager"
+#require_relative "../manager"
+require 'debugger'
 
 module SSH
   module Manager
     class Client
-      def self.list
-        puts 'list'
+
+      @list_arr = {1 => ["192.168.0.1", "foo"],
+                   2 => ["192.168.0.2", "root"],
+                   3 => ["192.168.0.3", "bar"]
+                  }
+
+      # List all entries
+      def self.list_all(list)
+        (1..list.size).each do |x|
+          puts "#{x}: #{list[x]}"
+        end
+      end
+
+      # Connect to specified connection with optional params
+      def self.connect(ip, user, param=[] )
+        ip.to_i
+        #add params
+        %x(xfce4-terminal --command="ssh #{user}@#{ip}")
       end
     end
   end
