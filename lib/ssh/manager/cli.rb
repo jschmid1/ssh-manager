bd1b0e3b6cd2a81d0ee9bf78174fd1b4ee12c23b
Joshua Schmid <jschmid@suse.com>
2014-10-22 12:22:19 +0200

    add db call get_all_connections

--- lib/ssh/manager/cli.rb
+++ lib/ssh/manager/cli.rb
@@ -4,4 +4,4 @@ require 'debugger'
 #debugger
 
 @connections = SSH::Manager::Database.new.get_connection_data
-@connections.map{|x| puts "#{x} \n"}
+@connections.map{|x| "#{x.map {|y| print "#{y} "}}"}
